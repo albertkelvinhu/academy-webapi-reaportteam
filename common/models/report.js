09013e803f83d1cd00911384d31530e05ccea5a5
albertkelvinhu <albertkelvinhutapea@gmail.com>
2017-07-18 14:46:38 +0700

    getProject per account
    dilakukan perubahan pada model report.

--- common/models/report.js
+++ common/models/report.js
@@ -51,6 +51,90 @@ module.exports = function(Report) {
         returns: {arg: "count", type: "number"}
   })
 
+    Report.getProject = function(task_id,cb){
+       app.models.Project.findOne({
+           include:{
+                relation: 'tasks', // include the owner object
+                scope: { // further filter the owner object
+                    where: {id: task_id}
+                }
+           }
+        },function(err, project){
+        if(err || task_id === 0)
+            return cb(err);
+        else {
+            console.log(project);
+            cb(null, project);
+        }
+        }) 
+    };
+
+    Report.remoteMethod("getProject",
+    {
+        accepts: [{ arg: 'task_id', type: 'string'}],
+        http: { path:"/:task_id/project", verb: "get", errorStatus: 401,},
+        description: ["Get get project."],
+        returns: {arg: "Project", type: "array"}
+    })
+
+    Report.getProjectbyUser = function(user_id,cb){
+        app.models.Project.find({
+           include:{
+                relation: 'tasks', // include the owner object
+                scope: { // further filter the owner object
+                    relation: 'assignment', // include the owner object
+                    scope: { // further filter the owner object
+                        where: {accountId: user_id}
+                    }
+                }
+           }
+        },function(err, project){
+        if(err || user_id === 0)
+            return cb(err);
+        else {
+            console.log(project);
+            cb(null, project);
+        }
+        }) 
+    };
+
+    Report.remoteMethod("getProjectbyUser",
+    {
+        accepts: [{ arg: 'user_id', type: 'string'}],
+        http: { path:"/:user_id/project", verb: "get", errorStatus: 401,},
+        description: ["Get project which an account is involved in."],
+        returns: {arg: "Project", type: "array"}
+    })
+
+    // Report.countProjectbyUser = function(user_id,cb){
+    //     app.models.Project.find({
+    //        include:{
+    //             relation: 'tasks', // include the owner object
+    //             scope: { // further filter the owner object
+    //                 relation: 'assignment', // include the owner object
+    //                 scope: { // further filter the owner object
+    //                     where: {accountId: user_id}
+    //                 }
+    //             }
+    //        }
+    //     },function(err, count){
+    //     if(err || user_id === 0)
+    //         return cb(err);
+    //     else {
+    //         console.log(count);
+    //         cb(null, count);
+    //     }
+    //     }) 
+    // };
+
+    // Report.remoteMethod("countProjectbyUser",
+    // {
+    //     accepts: [{ arg: 'user_id', type: 'string'}],
+    //     http: { path:"/:user_id/project", verb: "get", errorStatus: 401,},
+    //     description: ["Get project which an account is involved in."],
+    //     returns: {arg: "count", type: "number"}
+    // })
+
 
 //   Report.getAccountTasks = function(account_id,cb){
 //         app.models.Task.find({where: {AssignmentaccountId: account_id}},function(err, Tasks){
